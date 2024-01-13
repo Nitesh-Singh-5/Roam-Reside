d905b6c820a35260a62000b5017e5c7cf0a3d4ac
Nitesh Singh <ns98738698@gmail.com>
2024-01-14 00:40:43 +0530

    feat: add navbar and categories

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+
+  return (
+    <Image onClick={() => router.push("/")} className='hidden md:block cursor-pointer' src='/images/logo.png' height='150' width='150' alt='Logo' />
+  );
+};
+
+export default Logo;
