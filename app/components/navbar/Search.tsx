f17397d3f869a8a633aefb92b4531373044f61a5
Nitesh Singh <ns98738698@gmail.com>
2024-01-14 13:02:57 +0530

    feat: add loading screen and avatar

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -5,13 +5,13 @@ import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import { differenceInDays } from "date-fns";
 
-// import useSearchModal from "@/app/hooks/useSearchModal";
-// import useCountries from "@/app/hooks/useCountries";
+import useSearchModal from "@/app/hooks/useSearchModal";
+import useCountries from "@/app/hooks/useCountries";
 
 const Search = () => {
-  // const searchModal = useSearchModal();
+  const searchModal = useSearchModal();
   const params = useSearchParams();
-  // const { getByValue } = useCountries();
+  const { getByValue } = useCountries();
 
   const locationValue = params?.get("locationValue");
   const startDate = params?.get("startDate");
@@ -20,14 +20,11 @@ const Search = () => {
 
   const locationLabel = useMemo(() => {
     if (locationValue) {
-      // return getByValue(locationValue as string)?.label;
+      return getByValue(locationValue as string)?.label;
     }
 
     return "Anywhere";
-  }, [
-    locationValue,
-    // getByValue
-  ]);
+  }, [locationValue, getByValue]);
 
   const durationLabel = useMemo(() => {
     if (startDate && endDate) {
@@ -54,10 +51,7 @@ const Search = () => {
   }, [guestCount]);
 
   return (
-    <div
-      // onClick={searchModal.onOpen}
-      className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'
-    >
+    <div onClick={searchModal.onOpen} className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
       <div className='flex flex-row items-center justify-between'>
         <div className='text-sm font-semibold px-6'>{locationLabel}</div>
         <div className='hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center'>{durationLabel}</div>
