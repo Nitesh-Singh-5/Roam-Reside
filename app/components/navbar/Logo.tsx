9fced45483e307d28d82c7954790f807bb820dd4
Nitesh Singh <ns98738698@gmail.com>
2024-01-16 23:03:37 +0530

    Add mongodb with prisma and fix logo image naming

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -5,7 +5,6 @@ import { useRouter } from "next/navigation";
 
 const Logo = () => {
   const router = useRouter();
-
   return (
     <Image onClick={() => router.push("/")} className='hidden md:block cursor-pointer' src='/images/logo.png' height='150' width='150' alt='Logo' />
   );
